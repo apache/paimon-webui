9ddedabae4bf432f4847c03472f5842ea342bfc2
labbomb <739955946@qq.com>
2023-09-07 11:41:44 +0800

    feat: add license

--- paimon-web-ui-new/vite.config.ts
+++ paimon-web-ui-new/vite.config.ts
@@ -1,3 +1,20 @@
+/* Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License. */
+
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
@@ -28,7 +45,7 @@ export default defineConfig({
       ],
       dts: './auto-imports.d.ts',
       eslintrc: {
-        enabled: false, // 1、true时生成eslint配置文件，2、生成后改为false，避免重复消耗
+        enabled: false,
       },
     }),
     Components({
