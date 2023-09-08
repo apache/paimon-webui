2d2cc11ea3cfadf92e7952eb4223290319bc2275
labbomb <xujunjie@apache.org>
2023-09-08 12:48:02 +0800

    [Feature] Baseline implementation of Vue UI framework (#36)

---
+++ paimon-web-ui-new/src/main.ts
@@ -0,0 +1,49 @@
+/* Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License. */
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
+import App from './App'
+import router from './router'
+import naive from 'naive-ui'
+import { Setting } from './config'
+import './assets/tailMain.css'
+
+const app = createApp(App)
+
+const pinia = createPinia()
+app.use(pinia)
+pinia.use(piniaPluginPersistedstate)
+app.use(router)
+app.use(naive)
+
+app.mount('#app')
+
+console.clear();
+
+console.log(
+  `%c© Msi%c(Integration)\n%c${Setting.version}`,
+  `padding:24px 0px 12px 0px;
+  font-size:24px;
+  color:#66CCFF;`,
+  'font-size:24px;color:#FFE212;',
+  'font-size:12px;color:#FFBFCB;padding-bottom:24px;',
+);
+
+console.group('%cConsole', 'font-size:12px;color:#39C5BB;font-weight:300;');
---
+++ paimon-web-ui-new/src/views/HomePage.tsx
@@ -0,0 +1,40 @@
+/* Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License. */
+
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HomePage',
+  setup() {
+    const handleRequest = () => {}
+
+    return { handleRequest }
+  },
+  render() {
+    return (
+      <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
+          <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Welcome to Paimon</h2>
+        </div>
+        <div class="flex justify-center py-12">
+          <n-button onClick={this.handleRequest}>Request Test</n-button>
+        </div>
+      </div>
+    );
+  },
+});
