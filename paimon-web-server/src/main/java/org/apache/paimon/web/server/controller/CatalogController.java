91f7681626cbb092fe239532f9a507a70c10a6dd
hunter-cloud09 <liuhunter09@gmail.com>
2023-10-23 14:24:48 +0800

    feat: feat import *

--- paimon-web-server/src/main/java/org/apache/paimon/web/server/controller/CatalogController.java
+++ paimon-web-server/src/main/java/org/apache/paimon/web/server/controller/CatalogController.java
@@ -28,7 +28,13 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import java.util.List;
 
