e5f1361bf2c39082e874ee9a54961346e76e9c50
ZackYoung <zackyoungh@163.com>
2023-10-27 10:21:20 +0800

    spotless

--- paimon-web-server/src/main/java/org/apache/paimon/web/server/data/enums/CatalogMode.java
+++ paimon-web-server/src/main/java/org/apache/paimon/web/server/data/enums/CatalogMode.java
@@ -15,6 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.paimon.web.server.data.enums;
 
 /** Enum representing different catalog modes. */
