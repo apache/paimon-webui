d527c9102dbef02edf29c5104bb7f92d5e503504
ZackYoung <zackyoungh@163.com>
2023-11-09 20:50:06 +0800

    format_license

--- paimon-web-common/src/main/java/org/apache/paimon/web/common/utils/ReflectUtil.java
+++ paimon-web-common/src/main/java/org/apache/paimon/web/common/utils/ReflectUtil.java
@@ -7,7 +7,7 @@
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -21,9 +21,7 @@ package org.apache.paimon.web.common.utils;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 
-/**
- * reflect util
- */
+/** reflect util */
 public class ReflectUtil {
 
     /** Get the value of a static field */
