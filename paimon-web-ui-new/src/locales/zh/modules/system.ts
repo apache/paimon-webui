9f13c378b4ff42489e434dad2dee60f86db08f85
yangyang zhong <35210666+hdygxsj@users.noreply.github.com>
2023-12-14 22:53:09 +0800

    [feature][ui] role page

---
+++ paimon-web-ui-new/src/locales/zh/modules/system.ts
@@ -0,0 +1,31 @@
+/* Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License. */
+
+const user = {
+  add: '新增',
+  username: '用户名',
+  nickname: '昵称',
+  mobile: '手机号',
+  email: '邮箱'
+}
+
+const role = {
+  role_name: '角色名称',
+  role_key: '角色编码',
+  enabled: '是否启用'
+}
+export default { user, role }
