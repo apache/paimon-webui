9889e4680deb963a6b8fde958f19f55b2211a797
zhangmo8 <wegi866@gmail.com>
2023-12-28 23:36:49 +0800

    [Chore]: makeup lisence

--- paimon-web-ui-new/src/form-lib/metadata/use-database.ts
+++ paimon-web-ui-new/src/form-lib/metadata/use-database.ts
@@ -6,7 +6,7 @@ to you under the Apache License, Version 2.0 (the
 "License"); you may not use this file except in compliance
 with the License.  You may obtain a copy of the License at
 
-  http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing,
 software distributed under the License is distributed on an
@@ -18,7 +18,7 @@ under the License. */
 import type { IJsonItem } from '@/components/dynamic-form/types'
 
 export function useDatabase() {
-  const { t } = useLocaleHooks()
+	const { t } = useLocaleHooks()
 
 	const model = reactive({})
 
