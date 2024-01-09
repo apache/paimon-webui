8b60930a571fb712dea68416f77c05708ea65df5
s7monk <15512826113@163.com>
2024-01-09 23:20:03 +0800

    Complete the common module of paimon-web-engine-flink.

---
+++ paimon-web-engine/paimon-web-engine-flink/paimon-web-engine-flink-common/src/main/java/org/apache/paimon/web/engine/flink/common/status/JobStatus.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.paimon.web.engine.flink.common.status;
+
+/** Represents the various states that a job. */
+public enum JobStatus {
+    CREATED("CREATED"),
+    RUNNING("RUNNING"),
+    FINISHED("FINISHED"),
+    FAILED("FAILED"),
+    CANCELED("CANCELED");
+
+    private final String value;
+
+    public static JobStatus fromValue(String value) {
+        for (JobStatus type : values()) {
+            if (type.getValue().equals(value)) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("Unknown JobStatus value: " + value);
+    }
+
+    JobStatus(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+}
