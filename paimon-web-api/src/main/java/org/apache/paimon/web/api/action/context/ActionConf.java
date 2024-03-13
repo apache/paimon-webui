30e5a44502347cec4bd39cab26a71fd6db4e6aff
yangyang zhong <35210666+hdygxsj@users.noreply.github.com>
2024-03-13 23:01:54 +0800

    Optimization

--- paimon-web-api/src/main/java/org/apache/paimon/web/api/action/context/ActionConf.java
+++ paimon-web-api/src/main/java/org/apache/paimon/web/api/action/context/ActionConf.java
@@ -23,7 +23,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/** */
+/** ActionConf. */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface ActionConf {
