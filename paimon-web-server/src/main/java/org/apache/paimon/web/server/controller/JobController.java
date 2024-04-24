ca0c1511cefa18cf187e6d30fdde4e50f4d37168
s7monk <15512826113@163.com>
2024-04-24 18:50:57 +0800

    commit.

--- paimon-web-server/src/main/java/org/apache/paimon/web/server/controller/JobController.java
+++ paimon-web-server/src/main/java/org/apache/paimon/web/server/controller/JobController.java
@@ -1,6 +1,27 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.paimon.web.server.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.paimon.web.server.data.dto.SessionDTO;
+import org.apache.paimon.web.server.service.SessionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +35,10 @@ public class JobController {
     private static final String BATCH_MODE = "Batch";
     private static final String SHOW_JOBS_STATEMENT = "SHOW JOBS";
 
+    @Autowired
+    SessionService sessionService;
 
+    public void submit() {
+        sessionService.createSession(new SessionDTO());
+    }
 }
--- paimon-web-server/src/main/java/org/apache/paimon/web/server/service/impl/SessionServiceImpl.java
+++ paimon-web-server/src/main/java/org/apache/paimon/web/server/service/impl/SessionServiceImpl.java
@@ -1,8 +1,23 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.paimon.web.server.service.impl;
 
-import cn.dev33.satoken.stp.StpUtil;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.paimon.web.engine.flink.sql.gateway.client.SqlGatewayClient;
 import org.apache.paimon.web.engine.flink.sql.gateway.model.SessionEntity;
 import org.apache.paimon.web.server.data.dto.SessionDTO;
@@ -10,6 +25,9 @@ import org.apache.paimon.web.server.data.vo.UserVO;
 import org.apache.paimon.web.server.service.SessionService;
 import org.apache.paimon.web.server.service.UserService;
 import org.apache.paimon.web.server.service.UserSessionManager;
+
+import cn.dev33.satoken.stp.StpUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,11 +41,9 @@ public class SessionServiceImpl implements SessionService {
     private static final Integer ACTIVE_STATUS = 1;
     private static final Integer INACTIVE_STATUS = 0;
 
-    @Autowired
-    private UserSessionManager sessionManager;
+    @Autowired private UserSessionManager sessionManager;
 
-    @Autowired
-    private UserService userService;
+    @Autowired private UserService userService;
 
     @Override
     public void createSession(SessionDTO sessionDTO) {
@@ -73,7 +89,7 @@ public class SessionServiceImpl implements SessionService {
             }
         } catch (Exception e) {
             log.error("Unexpected error during session heartbeat", e);
-           return INACTIVE_STATUS;
+            return INACTIVE_STATUS;
         }
         return ACTIVE_STATUS;
     }
