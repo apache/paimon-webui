698117546d784de7c79709c8afd4676f10cac54d
s7monk <34889415+s7monk@users.noreply.github.com>
2024-05-16 15:37:16 +0800

    [Feature] Introduce job submission interface (#222)

---
+++ paimon-web-server/src/main/java/org/apache/paimon/web/server/data/model/JobInfo.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.paimon.web.server.data.model;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/** Job table model. */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@TableName("job")
+public class JobInfo extends BaseModel {
+
+    private String jobId;
+
+    private String jobName;
+
+    private String type;
+
+    private String executeMode;
+
+    private String clusterId;
+
+    private Integer uid;
+
+    private String config;
+
+    private String statements;
+
+    private String status;
+
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
+
+    private static final long serialVersionUID = 1L;
+}
