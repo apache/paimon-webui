8562603115e1689ce1bad8032f3568907ad710d7
s7monk <15512826113@163.com>
2024-05-22 18:37:38 +0800

    address comments.

--- paimon-web-ui/src/views/playground/components/query/components/console/components/controls/index.tsx
+++ paimon-web-ui/src/views/playground/components/query/components/console/components/controls/index.tsx
@@ -16,10 +16,9 @@ specific language governing permissions and limitations
 under the License. */
 
 import styles from './index.module.scss'
-import { TableChartOutlined, StopOutlined } from "@vicons/material"
+import { DataTable, Copy, Renew } from "@vicons/carbon"
+import { StopOutline } from "@vicons/ionicons5"
 import { LineChartOutlined, ClockCircleOutlined, DownloadOutlined } from "@vicons/antd"
-import {Refresh} from "@vicons/tabler"
-import {Copy20Regular} from "@vicons/fluent"
 import { useConfigStore } from '@/store/config'
 
 export default defineComponent({
@@ -47,81 +46,82 @@ export default defineComponent({
       <div class={styles.container}>
        <n-space class={styles.left} item-style="display: flex; align-items: center;">
          <n-popover trigger="hover" placement="bottom-start" show-arrow={false} style="padding: 0"
-                    v-slots={{
-                      trigger: () => (
-                        <n-button
-                          text
-                          class={[this.isDarkMode ? styles['active-button-dark'] : styles['active-button-light']]}
-                          onClick={() => this.setActiveButton('table')}
-                          v-slots={{
-                            icon: () => <n-icon component={TableChartOutlined}  size="20"></n-icon>
-                          }}
-                        >
-                        </n-button>
-                      )
-                    }}>
+            v-slots={{
+              trigger: () => (
+                <n-button
+                  text
+                  class={this.activeButton === 'table' ? styles['active-button'] : styles['table-action-bar-button']}
+                  onClick={() => this.setActiveButton('table')}
+                  v-slots={{
+                    icon: () => <n-icon component={DataTable}  size="20"></n-icon>
+                  }}
+                >
+                </n-button>
+              )
+            }}>
            <span>{this.t('playground.switch_to_table')}</span>
          </n-popover>
          <n-popover trigger="hover" placement="bottom-start" show-arrow={false}
-                    v-slots={{
-                      trigger: () => (
-                        <n-button
-                          text
-                          class={styles['table-action-bar-button']}
-                          v-slots={{
-                            icon: () => <n-icon component={LineChartOutlined}  size="20"></n-icon>
-                          }}
-                        >
-                        </n-button>
-                      )
-                    }}>
+            v-slots={{
+              trigger: () => (
+                <n-button
+                  text
+                  class={this.activeButton === 'chart' ? styles['active-button'] : styles['table-action-bar-button']}
+                  onClick={() => this.setActiveButton('chart')}
+                  v-slots={{
+                    icon: () => <n-icon component={LineChartOutlined}  size="20"></n-icon>
+                  }}
+                >
+                </n-button>
+              )
+            }}>
            <span>{this.t('playground.switch_to_chart')}</span>
          </n-popover>
          <n-divider vertical style="height: 20px; margin-left: 0px; margin-right: 0px; border-left-width: 3px;"/>
          <n-popover trigger="hover" placement="bottom-start" show-arrow={false}
-                    v-slots={{
-                      trigger: () => (
-                        <n-button
-                          text
-                          class={styles['table-action-bar-button']}
-                          v-slots={{
-                            icon: () => <n-icon component={Refresh}  size="20"></n-icon>
-                          }}
-                        >
-                        </n-button>
-                      )
-                    }}>
+            v-slots={{
+              trigger: () => (
+                <n-button
+                  text
+                  class={styles['table-action-bar-button']}
+                  v-slots={{
+                    icon: () => <n-icon component={Renew}  size="20"></n-icon>
+                  }}
+                >
+                </n-button>
+              )
+            }}>
            <span>{this.t('playground.refresh_data')}</span>
          </n-popover>
          <n-popover trigger="hover" placement="bottom-start" show-arrow={false}
-                    v-slots={{
-                      trigger: () => (
-                        <n-button
-                          text
-                          class={styles['table-action-bar-button']}
-                          style="color:  #D94F4F"
-                          v-slots={{
-                            icon: () => <n-icon component={StopOutlined} size="24"></n-icon>
-                          }}
-                        >
-                        </n-button>
-                      )
-                    }}>
+            v-slots={{
+              trigger: () => (
+                <n-button
+                  text
+                  class={styles['table-action-bar-button']}
+                  style="color:  #D94F4F"
+                  v-slots={{
+                    icon: () => <n-icon component={StopOutline} size="20"></n-icon>
+                  }}
+                >
+                </n-button>
+              )
+            }}>
            <span>{this.t('playground.stop_job')}</span>
          </n-popover>
          <n-popover trigger="hover" placement="bottom-start" show-arrow={false}
-                    v-slots={{
-                      trigger: () => (
-                        <n-button
-                          text
-                          class={styles['table-action-bar-button']}
-                          v-slots={{
-                            icon: () => <n-icon component={ClockCircleOutlined}  size="18.5"></n-icon>
-                          }}
-                        >
-                        </n-button>
-                      )
-                    }}>
+            v-slots={{
+              trigger: () => (
+                <n-button
+                  text
+                  class={styles['table-action-bar-button']}
+                  v-slots={{
+                    icon: () => <n-icon component={ClockCircleOutlined}  size="18.5"></n-icon>
+                  }}
+                >
+                </n-button>
+              )
+            }}>
            <span>{this.t('playground.schedule_refresh')}</span>
          </n-popover>
          <n-divider vertical style="height: 20px; margin-left: 0px; margin-right: 0px;"/>
@@ -133,33 +133,33 @@ export default defineComponent({
             <span class={styles['table-action-bar-text']}>Rows: 3</span>
             <span class={styles['table-action-bar-text']}>1m:06s</span>
             <n-popover trigger="hover" placement="bottom-start" show-arrow={false}
-                       v-slots={{
-                         trigger: () => (
-                           <n-button
-                             text
-                             class={styles['table-action-bar-button']}
-                             v-slots={{
-                               icon: () => <n-icon component={DownloadOutlined} size="20"></n-icon>
-                             }}
-                           >
-                           </n-button>
-                         )
-                       }}>
+               v-slots={{
+                 trigger: () => (
+                   <n-button
+                     text
+                     class={styles['table-action-bar-button']}
+                     v-slots={{
+                       icon: () => <n-icon component={DownloadOutlined} size="20"></n-icon>
+                     }}
+                   >
+                   </n-button>
+                 )
+               }}>
               <span>{this.t('playground.copy')}</span>
             </n-popover>
             <n-popover trigger="hover" placement="bottom-start" show-arrow={false}
-                       v-slots={{
-                         trigger: () => (
-                           <n-button
-                             text
-                             class={styles['table-action-bar-button']}
-                             v-slots={{
-                               icon: () => <n-icon component={Copy20Regular}  size="20"></n-icon>
-                             }}
-                           >
-                           </n-button>
-                         )
-                       }}>
+               v-slots={{
+                 trigger: () => (
+                   <n-button
+                     text
+                     class={styles['table-action-bar-button']}
+                     v-slots={{
+                       icon: () => <n-icon component={Copy}  size="20"></n-icon>
+                     }}
+                   >
+                   </n-button>
+                 )
+               }}>
               <span>{this.t('playground.download')}</span>
             </n-popover>
           </n-space>
