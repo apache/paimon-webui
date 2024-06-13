dff619074881f6807ee3655aebbd3f6097dbb44c
yangyang zhong <35210666+hdygxsj@users.noreply.github.com>
2024-06-14 00:46:27 +0800

    [Bugfix] Fix routing errors (#374)

--- paimon-web-ui/src/router/index.ts
+++ paimon-web-ui/src/router/index.ts
@@ -19,12 +19,12 @@ import {
   type NavigationGuardNext,
   type RouteLocationNormalized,
   createRouter,
-  createWebHistory,
+  createWebHashHistory,
 } from 'vue-router'
 import routes from './routes'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes,
 })
 
