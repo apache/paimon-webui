c33550774ade578ab4c37a4c5218a6e6b7ba8fec
yangyang zhong <35210666+hdygxsj@users.noreply.github.com>
2024-06-14 01:53:00 +0800

    [Improvement] UI Forward

--- paimon-web-server/src/main/java/org/apache/paimon/web/server/controller/PaimonWebUiForwardController.java
+++ paimon-web-server/src/main/java/org/apache/paimon/web/server/controller/PaimonWebUiForwardController.java
@@ -18,32 +18,20 @@
 
 package org.apache.paimon.web.server.controller;
 
-import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.http.HttpServletRequest;
-
-/** PaimonWebErrorController. */
+/** PaimonWebUiForwardController. */
 @Controller
-public class PaimonWebErrorController implements ErrorController {
+public class PaimonWebUiForwardController {
 
-    @RequestMapping("/error")
-    public ModelAndView handleError(HttpServletRequest request) {
-        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+    @GetMapping(value = "/ui/{[path:[^\\.]*}")
+    public ModelAndView forwardToIndex() {
         ModelAndView modelAndView = new ModelAndView();
-        if (status != null) {
-            Integer statusCode = Integer.valueOf(status.toString());
-            if (statusCode == HttpStatus.NOT_FOUND.value()) {
-                modelAndView.setStatus(HttpStatus.OK);
-                modelAndView.setViewName("forward:/ui/index.html");
-                return modelAndView;
-            }
-            modelAndView.setStatus(HttpStatus.valueOf(statusCode));
-        }
+        modelAndView.setStatus(HttpStatus.OK);
+        modelAndView.setViewName("forward:/ui/index.html");
         return modelAndView;
     }
 }
