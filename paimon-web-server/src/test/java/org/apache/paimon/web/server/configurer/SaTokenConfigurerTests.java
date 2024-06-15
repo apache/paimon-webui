d3f3994b0bbce8e70e9c2bc92cfeb8e5978136f3
silenceland <106915934+silenceland@users.noreply.github.com>
2024-06-15 12:15:46 +0800

    [Improvement] Introduce interceptor.exclude.path.patterns to exclude path patterns for configurer interceptor (#397)

--- paimon-web-server/src/test/java/org/apache/paimon/web/server/configurer/SaTokenConfigurerTests.java
+++ paimon-web-server/src/test/java/org/apache/paimon/web/server/configurer/SaTokenConfigurerTests.java
@@ -16,26 +16,31 @@
  * limitations under the License.
  */
 
-package org.apache.paimon.web.server.configrue;
+package org.apache.paimon.web.server.configurer;
 
-import cn.dev33.satoken.interceptor.SaInterceptor;
-import cn.dev33.satoken.stp.StpUtil;
+import org.apache.paimon.web.server.configrue.SaTokenConfigurer;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 
-/** Sa-Token path config. */
-@Configuration
-public class SaTokenConfigurer implements WebMvcConfigurer {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/** Test for {@link SaTokenConfigurer}. */
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SaTokenConfigurerTests {
 
     @Value("${interceptor.exclude.path.patterns}")
     private String[] excludePathPatterns;
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new SaInterceptor(handle -> StpUtil.checkLogin()))
-                .addPathPatterns("/**")
-                .excludePathPatterns(excludePathPatterns);
+    @Test
+    @Order(1)
+    public void testPathExclude() throws Exception {
+        assertEquals(excludePathPatterns.length, 2);
+        assertEquals(excludePathPatterns[0], "/api/login");
+        assertEquals(excludePathPatterns[1], "/ui/**");
     }
 }
