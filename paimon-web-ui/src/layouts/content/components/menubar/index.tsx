9a8df34f8e088b6e252a980988d8ce240ffba338
s7monk <15512826113@163.com>
2024-06-28 01:27:39 +0800

    commit.

--- paimon-web-ui/src/layouts/content/components/menubar/index.tsx
+++ paimon-web-ui/src/layouts/content/components/menubar/index.tsx
@@ -37,7 +37,7 @@ export default defineComponent({
     }
 
     onMounted(() => {
-      activeKey.value = configStore.getCurrentNavActive
+      activeKey.value = configStore.getCurrentNavActive!
     })
 
     return {
