cf4fc5d5b6f539fa1bd405da817fbac778370b82
XianmingZhou00 <147145809+XianmingZhou00@users.noreply.github.com>
2024-07-11 11:24:32 +0800

    [Improvement] Unify global date format (#496)

--- paimon-web-server/src/test/java/org/apache/paimon/web/server/configurer/JacksonConfigTests.java
+++ paimon-web-server/src/test/java/org/apache/paimon/web/server/configurer/JacksonConfigTests.java
@@ -26,7 +26,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -37,20 +44,24 @@ public class JacksonConfigTests {
 
     @Autowired private ObjectMapper objectMapper;
 
-    private static final String TIME_FORMATTED_JSON_STR = "\"2024-06-26 13:01:30\"";
-    private static final String TIME_UNFORMATTED_JSON_STR = "\"2024-06-26T13:01:30Z\"";
+    private static final String DATE_TIME_FORMATTED_JSON_STR = "\"2024-06-26 13:01:30\"";
+    private static final String DATE_TIME_UNFORMATTED_JSON_STR = "\"2024-06-26T13:01:30Z\"";
+    private static final String DATE_FORMATTED_JSON_STR = "\"2024-06-26\"";
+    private static final String TIME_FORMATTED_JSON_STR = "\"13:01:30\"";
+    private static final String DATE_TIME_STR = "2024-06-26 13:01:30";
+    private static final String DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
 
     @Test
     public void testLocalDateTimeSerialization() throws IOException {
         LocalDateTime dateTime = LocalDateTime.of(2024, 6, 26, 13, 1, 30);
         String json = objectMapper.writeValueAsString(dateTime);
-        assertEquals(TIME_FORMATTED_JSON_STR, json);
+        assertEquals(DATE_TIME_FORMATTED_JSON_STR, json);
     }
 
     @Test
     public void testLocalDateTimeDeserialization() throws IOException {
         LocalDateTime dateTime =
-                objectMapper.readValue(TIME_FORMATTED_JSON_STR, LocalDateTime.class);
+                objectMapper.readValue(DATE_TIME_FORMATTED_JSON_STR, LocalDateTime.class);
         assertEquals(LocalDateTime.of(2024, 6, 26, 13, 1, 30), dateTime);
     }
 
@@ -58,6 +69,58 @@ public class JacksonConfigTests {
     public void testInvalidLocalDateTimeDeserialization() {
         assertThrows(
                 com.fasterxml.jackson.databind.exc.InvalidFormatException.class,
-                () -> objectMapper.readValue(TIME_UNFORMATTED_JSON_STR, LocalDateTime.class));
+                () -> objectMapper.readValue(DATE_TIME_UNFORMATTED_JSON_STR, LocalDateTime.class));
+    }
+
+    @Test
+    public void testLocalDateSerialization() throws IOException {
+        LocalDate dateTime = LocalDate.of(2024, 6, 26);
+        String json = objectMapper.writeValueAsString(dateTime);
+        assertEquals(DATE_FORMATTED_JSON_STR, json);
+    }
+
+    @Test
+    public void testLocalDateDeserialization() throws IOException {
+        LocalDate dateTime = objectMapper.readValue(DATE_FORMATTED_JSON_STR, LocalDate.class);
+        assertEquals(LocalDate.of(2024, 6, 26), dateTime);
+    }
+
+    @Test
+    public void testLocalTimeSerialization() throws IOException {
+        LocalTime dateTime = LocalTime.of(13, 1, 30);
+        String json = objectMapper.writeValueAsString(dateTime);
+        assertEquals(TIME_FORMATTED_JSON_STR, json);
+    }
+
+    @Test
+    public void testLocalTimeDeserialization() throws IOException {
+        LocalTime dateTime = objectMapper.readValue(TIME_FORMATTED_JSON_STR, LocalTime.class);
+        assertEquals(LocalTime.of(13, 1, 30), dateTime);
+    }
+
+    @Test
+    public void testDateSerialization() throws IOException {
+        Date date = parseDate(DATE_TIME_STR);
+        String json = objectMapper.writeValueAsString(date);
+        assertEquals(DATE_TIME_FORMATTED_JSON_STR, json);
+    }
+
+    @Test
+    public void testDateDeserialization() throws IOException {
+        Date date = parseDate(DATE_TIME_STR);
+        Date dateDeserialized = objectMapper.readValue(DATE_TIME_FORMATTED_JSON_STR, Date.class);
+        assertEquals(dateDeserialized, date);
+    }
+
+    private Date parseDate(String date) {
+        DateTimeFormatter formatter =
+                DateTimeFormatter.ofPattern(JacksonConfigTests.DATE_TIME_PATTERN);
+
+        LocalDateTime localDateTime = LocalDateTime.parse(date, formatter);
+        ZoneId zoneId = ZoneId.systemDefault();
+        ZonedDateTime zonedDateTime = localDateTime.atZone(zoneId);
+
+        Instant instant = zonedDateTime.toInstant();
+        return Date.from(instant);
     }
 }
