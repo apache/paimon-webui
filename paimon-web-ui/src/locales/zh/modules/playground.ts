44dbd903efd48f78af11c828ea327ab92a95337c
Nicholas Jiang <programgeek@163.com>
2024-07-12 14:55:20 +0800

    Update paimon-web-ui/src/locales/zh/modules/playground.ts

--- paimon-web-ui/src/locales/zh/modules/playground.ts
+++ paimon-web-ui/src/locales/zh/modules/playground.ts
@@ -59,7 +59,7 @@ export default {
   job_stopping_failed: '停止任务失败',
   data_copied_successfully: '数据已复制到剪贴板',
   execution_mode: '运行模式',
-  engine: '执行引擎',
+  execution_engine: '执行引擎',
   deployment_cluster: '部署集群',
   limit_records: '限制行数',
 }
